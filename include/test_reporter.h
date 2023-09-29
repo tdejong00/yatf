318e70667e889f53f1f3d49fd1cf54848ac82489
T. de Jong <tdejong0212@gmail.com>
2023-09-29 22:51:35 +0200

    Add test reporter

---
+++ include/test_reporter.h
@@ -0,0 +1,92 @@
+#ifndef __TEST_REPORTER_H__
+#define __TEST_REPORTER_H__
+
+#include <cstdarg>
+#include <vector>
+#include <string>
+
+#include "test_runner.h"
+
+namespace test_framework {
+
+    /**
+     * @brief Represents the result of a test case.
+     */
+    enum test_result {
+        unknown, /* The result has not been set. */
+        failed, /* The test case has failed. */
+        passed /* The test case has passed. */
+    };
+
+    /**
+     * @brief Contains statistics about the executed test cases.
+     * 
+     */
+    struct test_statistics {
+        int n_total; /* Total number of test cases. */
+        int n_passed; /* Number of test cases that have passed. */
+        int n_failed; /* Number of test cases that have failed. */
+    };
+
+    /**
+     * @brief Provides functionality for announcing tests and reporting test results.
+     */
+    class test_reporter {
+        public:
+            /**
+             * @brief Announces all test suites.
+             * 
+             * @param test_suites A list of test suites to announce.
+             */
+            static void announce(std::vector<test_suite> test_suites);
+
+            /**
+             * @brief Announces a single test suite.
+             * 
+             * @param test_suite The test suite to announce.
+             */
+            static void announce(test_suite test_suite);
+
+            /**
+             * @brief Reports the passing of a test case.
+             * 
+             * @param test_case The test case to report.
+             */
+            static void report(test_case test_case);
+
+            /**
+             * @brief Reports the failure of a test case.
+             * 
+             * @param test_case The test case to report.
+             * @param failure_reason Reason for why the test case has failed.
+             */
+            static void report(test_case test_case, const char *failure_reason);
+
+            /**
+             * @brief Reports the result of all test cases.
+             * 
+             * @param test_statistics The statistics for the executed tests.
+             */
+            static void report(test_statistics test_statistics);
+
+        private:
+            /**
+             * @brief Converts a test result to its string representation.
+             * 
+             * @param test_result The test result to convert.
+             * @return The string representation of the test result.
+             */
+            static std::string to_string(test_result test_result);
+
+            /**
+             * Logs a formatted message for the specified result.
+             * 
+             * @param result The test result.
+             * @param format Specifies the structure of the log message.
+             * @param ... Additional arguments to be formatted into the message.
+             */
+            static void log(test_result test_result, const char *format, ...);
+    };
+}
+
+#endif
---
+++ src/test_reporter.cc
@@ -0,0 +1,48 @@
+#include "test_reporter.h"
+
+namespace test_framework {
+    void test_reporter::announce(std::vector<test_suite> test_suites) {
+        size_t n_suites = test_suites.size();
+        int n_tests = 0;
+        for (test_suite test_suite : test_suites) {
+            n_tests += test_suite.test_cases.size();
+        }
+        log(unknown, "Running %ld tests from %ld files...", n_tests, n_suites);
+    }
+
+    void test_reporter::announce(test_suite test_suite) {
+        log(unknown, "Running %ld tests from %s:", test_suite.test_cases.size(), test_suite.name);
+    }
+
+    void test_reporter::report(test_case test_case) {
+        log(passed, "%s:%d:%s", test_case.file_name, test_case.lineno, test_case.name);
+    }
+
+    void test_reporter::report(test_case test_case, const char *failure_reason) {
+        log(failed, "%s:%d:%s -> %s", test_case.file_name, test_case.lineno, test_case.name, failure_reason);
+    }
+
+    void test_reporter::report(test_statistics test_statistics) {
+        log(unknown, "%d tests passed, %d tests failed", test_statistics.n_passed, test_statistics.n_failed);
+    }
+
+    std::string test_reporter::to_string(test_result test_result) {
+        static const std::string reset_color = "\033[0;39m";
+        static const std::string red_color = "\033[31m";
+        static const std::string green_color = "\033[32m";
+        switch (test_result) {
+            case test_result::failed: return red_color + "[Failed]" + reset_color;
+            case test_result::passed: return green_color + "[Passed]" + reset_color;
+            default: return green_color + "[------]" + reset_color;
+        }
+    }
+
+    void test_reporter::log(test_result test_result, const char *format, ...) {
+        va_list args;
+        va_start(args, format);
+        printf("%s: ", to_string(test_result).c_str());
+        vprintf(format, args);
+        printf("\n");
+        va_end(args);
+    }
+}
